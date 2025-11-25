f3de1dd77a7f3b5523b5b65090d33d28cd8373ca
Eucalypt <eluvk.dev@gmail.com>
2025-11-25 17:05:22 +0800

    .

--- syncstore_client/example/main.dart
+++ syncstore_client/example/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path/path.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:syncstore_client/syncstore_client.dart';
 
 part 'main.g.dart';
@@ -25,6 +30,7 @@ void main() async {
         final firstRepoId = list.items.first.id;
         final DataItem<Repo> repoItem = await client.get<Repo>('xbb', 'repo', firstRepoId, Repo.fromJson);
         print('fetched repo by id: ${repoItem.toJson((Repo r) => r.toJson())}');
+        await RepoRepository().addToLocalDb(repoItem);
       }
 
       // delete repos
@@ -48,13 +54,124 @@ void main() async {
   }
 }
 
-@JsonSerializable()
-class Repo {
+abstract class TestDBBasic {
+  Future<Database> getDb();
+}
+
+@JsonSerializable(includeIfNull: false)
+class Repo extends TestDBBasic {
   String name;
   String status;
+  String? description;
 
-  Repo({required this.name, required this.status});
+  Repo({required this.name, required this.status, this.description});
 
   factory Repo.fromJson(Map<String, dynamic> json) => _$RepoFromJson(json);
   Map<String, dynamic> toJson() => _$RepoToJson(this);
+
+  @override
+  Future<Database> getDb() async {
+    return await TestDataBase().getDb();
+  }
+}
+
+extension SSRepo on Repo {
+  static String get tableName => 'repo';
+  static String get onCreateTableRepoSQL =>
+      '''
+        CREATE TABLE ${SSRepo.tableName} (
+          id TEXT PRIMARY KEY,
+          created_at TEXT NOT NULL,
+          updated_at TEXT NOT NULL,
+          owner TEXT NOT NULL,
+          parent_id TEXT,
+          "unique" TEXT,
+          body TEXT NOT NULL
+        )
+      ''';
+  static Future<Database> DB() async {
+    return await TestDataBase().getDb();
+  }
+}
+
+typedef RepoDataItem = DataItem<Repo>;
+
+class RepoRepository {
+  Future<void> addToLocalDb(RepoDataItem item) async {
+    final db = await SSRepo.DB();
+    await db.insert(SSRepo.tableName, item.toJson((Repo r) => json.encode(r.toJson())));
+  }
+
+  Future<RepoDataItem?> getFromLocalDb(String id) async {
+    final db = await SSRepo.DB();
+    final List<Map<String, dynamic>> maps = await db.query(SSRepo.tableName, where: 'id = ?', whereArgs: [id]);
+    if (maps.isNotEmpty) {
+      print('maps first: ${maps.first}');
+      return DataItem<Repo>.fromJson(maps.first, (jsonStr) => Repo.fromJson(json.decode(jsonStr as String)));
+    }
+    return null;
+  }
+
+  Future<List<RepoDataItem>> listFromLocalDb({String? parentId}) async {
+    final db = await SSRepo.DB();
+    final whereClauses = <String>[];
+    final whereArgs = <dynamic>[];
+    if (parentId != null) {
+      whereClauses.add('parent_id = ?');
+      whereArgs.add(parentId);
+    }
+    final whereString = whereClauses.isNotEmpty ? whereClauses.join(' AND ') : null;
+    final List<Map<String, dynamic>> maps = await db.query(SSRepo.tableName, where: whereString, whereArgs: whereArgs);
+    return maps
+        .map((map) => DataItem<Repo>.fromJson(map, (jsonStr) => Repo.fromJson(json.decode(jsonStr as String))))
+        .toList();
+  }
+
+  Future<void> deleteFromLocalDb(String id) async {
+    final db = await SSRepo.DB();
+    await db.delete(SSRepo.tableName, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<void> updateToLocalDb(RepoDataItem item) async {
+    final db = await SSRepo.DB();
+    await db.update(
+      SSRepo.tableName,
+      item.toJson((Repo r) => json.encode(r.toJson())),
+      where: 'id = ?',
+      whereArgs: [item.id],
+    );
+  }
+
+  Future<void> upsertToLocalDb(RepoDataItem item) async {
+    if (await getFromLocalDb(item.id) == null) {
+      await addToLocalDb(item);
+    } else {
+      await updateToLocalDb(item);
+    }
+  }
+}
+
+class TestDataBase {
+  static Database? _db;
+
+  Future<Database> getDb() async {
+    if (_db != null) return _db!;
+    if (!Platform.isAndroid && !Platform.isIOS) {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+    }
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'example_c.db');
+
+    _db ??= await databaseFactoryFfi.openDatabase(
+      path,
+      options: OpenDatabaseOptions(
+        version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute(SSRepo.onCreateTableRepoSQL);
+        },
+      ),
+    );
+    return _db!;
+  }
 }
--- syncstore_client/example/main.g.dart
+++ syncstore_client/example/main.g.dart
@@ -6,6 +6,11 @@ part of 'main.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Repo _$RepoFromJson(Map<String, dynamic> json) => Repo(name: json['name'] as String, status: json['status'] as String);
+Repo _$RepoFromJson(Map<String, dynamic> json) =>
+    Repo(name: json['name'] as String, status: json['status'] as String, description: json['description'] as String?);
 
-Map<String, dynamic> _$RepoToJson(Repo instance) => <String, dynamic>{'name': instance.name, 'status': instance.status};
+Map<String, dynamic> _$RepoToJson(Repo instance) => <String, dynamic>{
+  'name': instance.name,
+  'status': instance.status,
+  'description': ?instance.description,
+};
